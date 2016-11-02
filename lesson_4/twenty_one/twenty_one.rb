516892857886834da3a8139efcd277a903e5939a
PeriwinkleBluu <mitchelljoshj@gmail.com>
2016-11-02 02:04:14 +0000

    Added suggestions from code review

--- lesson_4/twenty_one/twenty_one.rb
+++ lesson_4/twenty_one/twenty_one.rb
@@ -1,46 +1,40 @@
 
-# methds to support playing 21
 require_relative 'twenty_one_methods'
-score = [0, 0]
+score = { player: 0, dealer: 0 }
 
 loop do
-  # initialize deck, player, and dealers hands
   current_deck = initialize_deck
   player_hand = []
   dealer_hand = []
-  # draw cards for player and dealer
+
   2.times do
-    player_hand << deal_card!(current_deck)
-    dealer_hand << deal_card!(current_deck)
+    deal_card!(current_deck, player_hand)
+    deal_card!(current_deck, dealer_hand)
   end
 
-  # player hit or stay loop
   player_plays(player_hand, dealer_hand, current_deck)
 
-  # if the player hasn't busted the dealer begins playing
   if get_value(player_hand) <= MAX_VALUE
     dealer_plays(dealer_hand, current_deck)
   end
 
-  # get the winner and keep track of winners
   winner = get_winner(player_hand, dealer_hand)
   keep_score!(winner, score)
 
-  # Display the final hands and values for the player and the dealer,as well as the winner
   system 'clear'
-  puts "The player had #{display_cards(player_hand)} for #{get_value(player_hand)} points."
-  puts "The dealer had #{display_cards(dealer_hand)} for #{get_value(dealer_hand)} points."
-  puts "#{winner} The score is currently PLAYER:#{score[0]} DEALER:#{score[1]}"
-  # break out of the loop if either has won 5 times
-  if score[0] == 5
-    puts "The player has won 5 times!"
-    break
-  elsif score[1] == 5
-    puts "The dealer has won 5 times"
-    break
+  puts <<-DISPLAY_FINAL
+The player had #{display_cards(player_hand)} for #{get_value(player_hand)} points.
+The dealer had #{display_cards(dealer_hand)} for #{get_value(dealer_hand)} points.
+#{winner} The score is currently PLAYER:#{score[:player]} DEALER:#{score[:dealer]}
+  DISPLAY_FINAL
+
+  if score.value?(5)
+    puts <<-FINAL_SCORE
+The Player won #{score[:player]} times and
+The Dealer won #{score[:dealer]} times
+    FINAL_SCORE
   end
 
-  # See if the player wants to play again.
   puts "Would you like to play again? (Y)es or (N)o"
   answer = gets.chomp.downcase
   break unless answer[0] == 'y'
--- lesson_4/twenty_one/twenty_one_methods.rb
+++ lesson_4/twenty_one/twenty_one_methods.rb
@@ -3,19 +3,18 @@ SUITS = ["\u2660", "\u2661", "\u2662", "\u2663"].freeze
 MAX_VALUE = 21
 DEALER_STOP = 17
 
-# reset deck to full cards
 def initialize_deck
-  [["A", 0], ["2", 0], ["3", 0], ["4", 0], ["5", 0], ["6", 0], ["7", 0]] +
-    [["8", 0], ["9", 0], ["10", 0], ["J", 0], ["Q", 0], ["K", 0]] +
-    [["A", 1], ["2", 1], ["3", 1], ["4", 1], ["5", 1], ["6", 1], ["7", 1]] +
-    [["8", 1], ["9", 1], ["10", 1], ["J", 1], ["Q", 1], ["K", 1]] +
-    [["A", 2], ["2", 2], ["3", 2], ["4", 2], ["5", 2], ["6", 2], ["7", 2]] +
-    [["8", 2], ["9", 2], ["10", 2], ["J", 2], ["Q", 2], ["K", 2]] +
-    [["A", 3], ["2", 3], ["3", 3], ["4", 3], ["5", 3], ["6", 3], ["7", 3]] +
-    [["8", 3], ["9", 3], ["10", 3], ["J", 3], ["Q", 3], ["K", 3]]
+  suit_numbers = *(0..3)
+  values = *(2..10).map(&:to_s) + ["A", "K", "Q", "J"]
+  deck = []
+  suit_numbers.each do |s|
+    values.each do |v|
+      deck.push([v, s])
+    end
+  end
+  deck
 end
 
-# returns a string of cards for the user to read
 def display_cards(card_list)
   hand = []
   card_list.each do |card|
@@ -24,38 +23,26 @@ def display_cards(card_list)
   hand.join(" ")
 end
 
-# returns the value of the cards in the hand
 def get_value(card_list)
   value = 0
-  # calculate initial value
   card_list.each do |card|
-    # check for Ace
     value += if card[0] == 'A'
                11
-             # check for Royal
              elsif card[0].to_i.zero? # 'J', 'Q', 'K'
                10
-             # number
              else
                card[0].to_i
              end
   end
   # if value is over 21 and the player has an A recalculate
-  if value > MAX_VALUE && card_list.any? { |i| [["A", 0], ["A", 1], ["A", 2], ["A", 3]].include? i }
+  if value > MAX_VALUE
     value = calc_a_value(card_list, value)
   end
   value
 end
 
-# for calculating hand value if an ace is given and value is over 21
 def calc_a_value(card_list, value)
-  count = 0
-  # find how many aces
-  (0..3).each do |i|
-    if card_list.include?(["A", i])
-      count += 1
-    end
-  end
+  count = card_list.count { |i| i[0] == "A" }
   # turn the minimum required into 1's
   while value > MAX_VALUE && count > 0
     value -= 10
@@ -64,22 +51,21 @@ def calc_a_value(card_list, value)
   value
 end
 
-# removes a card from the deck and returns it
-def deal_card!(card_list)
-  card_list.delete_at rand card_list.length
+def deal_card!(card_list, hand)
+  hand << card_list.delete_at(rand(card_list.length))
 end
 
-# The logic for the players turn
 def player_plays(player_hand, dealer_hand, current_deck)
   loop do
     system 'clear'
-    puts "The dealer has #{display_cards([dealer_hand[0]])} and ??"
-    puts "Your hand is: #{display_cards(player_hand)}"
-    puts "Your hand is currently worth #{get_value(player_hand)} points."
-    puts 'Would you like to (h)it or (s)tay?'
-    answer = gets.chomp.downcase
-    break unless answer[0] == 'h'
-    player_hand << deal_card!(current_deck)
+    puts <<-CURR_GAMESTATE
+"The dealer has #{display_cards([dealer_hand[0]])} and ??
+Your hand is: #{display_cards(player_hand)}
+Your hand is currently worth #{get_value(player_hand)} points.
+Would you like to (h)it or (s)tay?
+    CURR_GAMESTATE
+    break unless gets.chomp.casecmp('h').zero?
+    deal_card!(current_deck, player_hand)
     if get_value(player_hand) > MAX_VALUE
       puts "You Busted!"
       break
@@ -87,14 +73,12 @@ def player_plays(player_hand, dealer_hand, current_deck)
   end
 end
 
-# dealers logic
 def dealer_plays(dealer_hand, current_deck)
   while get_value(dealer_hand) < DEALER_STOP
-    dealer_hand << deal_card!(current_deck)
+    deal_card!(current_deck, dealer_hand)
   end
 end
 
-# find out whether the player or dealer won
 def get_winner(player_hand, dealer_hand)
   deal_points = get_value(dealer_hand)
   player_points = get_value(player_hand)
@@ -110,8 +94,8 @@ end
 
 def keep_score!(winner, score)
   if winner == "The player wins!"
-    score[0] += 1
+    score[:player] += 1
   elsif winner == "The dealer wins!"
-    score[1] += 1
+    score[:dealer] += 1
   end
 end
